6df7804197c104b310eeb7c66fd2861ecd72bd0e
CanGroves <172975487@qq.com>
2023-02-25 15:41:08 +0800

    add comments

--- chapter2/guessing_game/src/main.rs
+++ chapter2/guessing_game/src/main.rs
@@ -18,12 +18,12 @@ fn main() {
         .expect("Failed to read line.");
 
     let guess: u32 = guess.trim() // delete the spare empty char at the beginning and end of String. eg. \r\n in Windows, \n in Linux.
-        .parse() // parse String to some type, specified by ': u32'. This method can get wrong by illegal string, thus it will return 'Result' type, and we use expect() to handle it.
-        .expect("Please type a number!");
+        .parse() // parse String to some type, specified by ': u32'. This method can get wrong by illegal string input, thus it will return 'Result' type, and we use expect() to handle it.
+        .expect("Please type a number!"); // this var 'guess' shadow the var 'guess' defined in line 14, to allow programmers not to use too many vars to express the same meaning with different types, such as 'guess_str' and 'guess_u32'.
 
     println!("You guessed : {}", guess);
 
-    match guess.cmp(&secret_number) { // rust can infer that secret_number should be a u32 by guess(u32).cmp().
+    match guess.cmp(&secret_number) { // rust can infer that secret_number should be a u32 by 'guess(u32).cmp()'.
         Ordering::Less => println!("Too small!"),
         Ordering::Greater => println!("Too big!"),
         Ordering::Equal => println!("You win!"),
