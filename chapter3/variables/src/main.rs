b23bdeca8fc9673ef373f8ce070eae4bcbcf83d2
CanGroves <172975487@qq.com>
2023-02-25 16:29:33 +0800

    practice shadow

--- chapter3/variables/src/main.rs
+++ chapter3/variables/src/main.rs
@@ -1,6 +1,21 @@
 fn main() {
-    let mut x = 5;
-    println!("x is {}", x);
-    x = 6;
-    println!("x is {}", x);
+    // let mut x = 5;
+    // println!("x is {}", x);
+    // x = 6;
+    // println!("x is {}", x);
+    let x = 5;
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("x in the inner scope is: {}", x);
+    }
+
+    println!("x in the outter scope is: {}", x);
+
+    let spaces = "      ";
+    let spaces = spaces.len();
+
+    println!("len of spaces is: {}", spaces);
+
 }
