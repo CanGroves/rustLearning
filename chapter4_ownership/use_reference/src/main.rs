fc4b3789885e15531b20124f365eb3700e349ef8
CanGroves <172975487@qq.com>
2023-02-28 23:48:57 +0800

    use reference

---
+++ chapter4_ownership/use_reference/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let s1 = String::from("hello");
+
+    let len = calculate_length(&s1);
+
+    println!("The length of {} is {}", s1, len);
+}
+
+fn calculate_length(s: &String) -> usize {
+    // s.push_str(", world"); // error: cannot borrow `*s` as mutable, as it is behind a `&` reference. `s` is a `&` reference, so the data it refers to cannot be borrowed as mutable
+    s.len()
+}
