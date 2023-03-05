279b8cc9db0ff3078affd69ca70626e543b9cf21
CanGroves <172975487@qq.com>
2023-03-05 14:16:41 +0800

    parctice immutable and mutable at the same time

--- chapter4_ownership/use_mutable_reference/src/main.rs
+++ chapter4_ownership/use_mutable_reference/src/main.rs
@@ -7,6 +7,19 @@ fn main() {
     println!("r1: {}", r1);
     let r2 = &mut s;
     println!("r2: {}", r2);
+
+    {
+        let r3 = &mut s;
+    }
+    let r4 = &mut s;
+
+    // immutable
+    let r5 = &s;
+    let r6 = &s;
+    // let r7 = &mut s; // error: cannot borrow `s` as mutable because it is also borrowed as immutable
+
+    // println!("{}, {}, and {}", r5, r6, r7); // error: immutable borrow later used at r5.
+    println!("{}, {}", r5, r6);
 }
 
 fn change(some_string: &mut String) {
