f80a0123c3bc0f6c6f787917b889ab405013ceb4
CanGroves <172975487@qq.com>
2023-03-08 21:22:42 +0800

    tuple struct

---
+++ chapter5_struct/tuple_struct/target/debug/deps/tuple_struct-2ac0ae386be83817.d
@@ -0,0 +1,5 @@
+c:\Users\user\Desktop\learnRust\chapter5_struct\tuple_struct\target\debug\deps\tuple_struct-2ac0ae386be83817.rmeta: src\main.rs
+
+c:\Users\user\Desktop\learnRust\chapter5_struct\tuple_struct\target\debug\deps\tuple_struct-2ac0ae386be83817.d: src\main.rs
+
+src\main.rs:
---
+++ chapter5_struct/tuple_struct/target/debug/deps/tuple_struct-3f87a47f36ec947b.d
@@ -0,0 +1,5 @@
+c:\Users\user\Desktop\learnRust\chapter5_struct\tuple_struct\target\debug\deps\tuple_struct-3f87a47f36ec947b.rmeta: src\main.rs
+
+c:\Users\user\Desktop\learnRust\chapter5_struct\tuple_struct\target\debug\deps\tuple_struct-3f87a47f36ec947b.d: src\main.rs
+
+src\main.rs:
