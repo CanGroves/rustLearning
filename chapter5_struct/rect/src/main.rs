ed778291556e72065d462454c74b411a0dca49b0
CanGroves <172975487@qq.com>
2023-03-09 21:54:00 +0800

    restruct more clearly by using struct

--- chapter5_struct/rect/src/main.rs
+++ chapter5_struct/rect/src/main.rs
@@ -1,3 +1,4 @@
+/* usaul calc of rect's area */
 // fn main() {
 //     let width1 = 30;
 //     let height1 = 50;
@@ -12,15 +13,37 @@
 //     width * height
 // }
 
+/* use tuple to calc of rect's area */
+// fn main() {
+//     let rect1 = (30, 50);
+
+//     println!(
+//         "The area of the rectangle is {} suqare pixels.",
+//         area(rect1)
+//     );
+// }
+
+// fn area(dimensions: (u32, u32)) -> u32 {
+//     dimensions.0 * dimensions.1
+// }
+
+/* use struct to calc of rect's area more clearly*/
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 fn main() {
-    let rect1 = (30, 50);
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
 
     println!(
         "The area of the rectangle is {} suqare pixels.",
-        area(rect1)
+        area(&rect1)
     );
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
