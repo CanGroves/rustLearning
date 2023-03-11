66ff92c4109b41ddf0752f025d8b2140050acdd3
CanGroves <172975487@qq.com>
2023-03-11 16:03:45 +0800

    more arguments in method

--- chapter5_struct/rect/src/main.rs
+++ chapter5_struct/rect/src/main.rs
@@ -38,10 +38,18 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, another_rect: &Rectangle) -> bool {
+        self.width >= another_rect.width && self.height >= another_rect.height
+    }
 }
 
 fn main() {
-    let scale = 2;
+    let scale = 1;
     let rect1 = Rectangle {
         width: dbg!(30 * scale), // dbg! will return the ownership of the value.
         height: 50,
@@ -54,9 +62,26 @@ fn main() {
 
     println!("rect1 is {:#?} by println", rect1);
 
+    if rect1.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    }
+
     dbg!(&rect1);
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 20,
+    };
+
+    let rect3 = Rectangle {
+        width: 30,
+        height: 60,
+    };
+
+    println!("Can rect1 hold rect2? : {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? : {}", rect1.can_hold(&rect3));
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
-}
+// fn area(rectangle: &Rectangle) -> u32 {
+//     rectangle.width * rectangle.height
+// }
